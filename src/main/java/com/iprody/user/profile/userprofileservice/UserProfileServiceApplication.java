99ddc18c177c4eb90d682f19b65c012bbbccb586
Mykyta <zakharenkomykyta41@gmail.com>
2023-10-30 15:16:00 +0200

    Establish Infrustructure + Establish Service
    Added .github/workflows/build-and-store-image-ghcr.yml .github/workflows/publish-artifact.yml .github/workflows/validate-project.yml Dockerfile config/ docker-compose.yml

--- src/main/java/com/iprody/user/profile/userprofileservice/UserProfileServiceApplication.java
+++ src/main/java/com/iprody/user/profile/userprofileservice/UserProfileServiceApplication.java
@@ -6,6 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserProfileServiceApplication {
 
+    /**
+     * Main method of application.
+     *
+     * @param args arguments of the command line.
+     *
+     */
+
     public static void main(String[] args) {
         SpringApplication.run(UserProfileServiceApplication.class, args);
     }
