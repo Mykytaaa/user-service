3193299b3c18ea4fcc1af533ee9fde7b88dabad2
Mykyta <zakharenkomykyta41@gmail.com>
2024-02-14 15:46:57 +0200

    Establish E2E Testing Application(second module)

---
+++ e2e/src/main/java/com/mykytaaa/user/profile/service/e2e/TestingClientApplication.java
@@ -0,0 +1,13 @@
+package com.mykytaaa.user.profile.service.e2e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingClientApplication.class, args);
+    }
+
+}
---
+++ e2e/src/test/resources/features/example.feature
@@ -0,0 +1,6 @@
+Feature: An example
+
+  Scenario:
+    Given I run a simple test
+    When The test is running
+    Then Always assert true
