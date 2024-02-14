196657d6d29870f696faf5251825f1e1f14feaee
Mykyta <zakharenkomykyta41@gmail.com>
2024-02-14 15:46:57 +0200

    Establish E2E Testing Application(second module)

---
+++ e2e/src/test/java/com/mykytaaa/user/profile/service/e2e/RunCucumberTest.java
@@ -0,0 +1,10 @@
+package com.mykytaaa.user.profile.service.e2e;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources")
+class RunCucumberTest {
+}
---
+++ e2e/src/test/java/com/mykytaaa/user/profile/service/e2e/config/WebClientConfig.java
@@ -0,0 +1,80 @@
+package com.mykytaaa.user.profile.service.e2e.config;
+
+import lombok.SneakyThrows;
+import org.apache.hc.client5.http.config.TlsConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.io.HttpClientConnectionManager;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactoryBuilder;
+import org.apache.hc.core5.http.ssl.TLS;
+import org.apache.hc.core5.ssl.SSLContextBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.SSLContext;
+
+@Configuration
+public class WebClientConfig {
+
+    /**
+     * keystore from resources folder.
+     */
+    private final Resource trustStore;
+
+    /**
+     * keystore password from `application.yml`.
+     */
+    private final String trustStorePassword;
+
+    /**
+     * Configurable variable to set TLS version.
+     */
+    private final TLS tlsVersion;
+
+    public WebClientConfig(
+            @Value("${user-profile-service.ssl.trust-store}") Resource trustStore,
+            @Value("${user-profile-service.ssl.trust-store-password}") String trustStorePassword,
+            @Value("${user-profile-service.ssl.version}") TLS tlsVersion
+    ) {
+        this.trustStore = trustStore;
+        this.trustStorePassword = trustStorePassword;
+        this.tlsVersion = tlsVersion;
+    }
+
+    /**
+     * Create bean RestTemplate.
+     *
+     * @return instance RestTemplate
+     */
+    @Bean
+    public RestTemplate restTemplate() {
+        final CloseableHttpClient httpClient = buildSslHttpClient();
+        final ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        return new RestTemplate(requestFactory);
+    }
+
+    @SneakyThrows
+    private CloseableHttpClient buildSslHttpClient() {
+        final SSLContext sslContext = new SSLContextBuilder()
+                .loadTrustMaterial(trustStore.getURL(), trustStorePassword.toCharArray()).build();
+        final SSLConnectionSocketFactory sslSocketFactory = SSLConnectionSocketFactoryBuilder.create()
+                .setSslContext(sslContext)
+                .build();
+        final HttpClientConnectionManager cm = PoolingHttpClientConnectionManagerBuilder.create()
+                .setSSLSocketFactory(sslSocketFactory)
+                .setDefaultTlsConfig(TlsConfig.custom()
+                        .setSupportedProtocols(tlsVersion)
+                        .build())
+                .build();
+        return HttpClients.custom()
+                .setConnectionManager(cm)
+                .build();
+    }
+}
